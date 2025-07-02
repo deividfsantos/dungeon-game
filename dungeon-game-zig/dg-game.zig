5bfca1a693345b86c3a90e21a1d39e2e550d120c
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-07-02 01:09:18 -0300

    almost working zig version

--- dungeon-game-zig/dg-game.zig
+++ dungeon-game-zig/dg-game.zig
@@ -1,35 +1,36 @@
 const std = @import("std");
 
-pub fn calculateMinimumHP(allocator: std.mem.Allocator, dungeon: [][]i32) !i32 {
+pub fn calculateMinimumHP(allocator: std.mem.Allocator, dungeon: anytype) !i32 {
     const m = dungeon.len;
     const n = dungeon[0].len;
 
     var dp = try allocator.alloc([]i32, m);
     defer allocator.free(dp);
 
-    for (dp) |*row, i| {
-        row.* = try allocator.alloc(i32, n);
-        defer allocator.free(row.*);
+    var k: usize = 0;
+    while (k < m) : (k += 1) {
+        dp[k] = try allocator.alloc(i32, n);
+        defer allocator.free(dp[k]);
     }
 
-    dp[m - 1][n - 1] = std.math.max(1, 1 - dungeon[m - 1][n - 1]);
+    dp[m - 1][n - 1] = @max(1, 1 - dungeon[m - 1][n - 1]);
 
     var i: usize = m - 1;
     while (i > 0) : (i -= 1) {
-        dp[i - 1][n - 1] = std.math.max(1, dp[i][n - 1] - dungeon[i - 1][n - 1]);
+        dp[i - 1][n - 1] = @max(1, dp[i][n - 1] - dungeon[i - 1][n - 1]);
     }
 
     var j: usize = n - 1;
     while (j > 0) : (j -= 1) {
-        dp[m - 1][j - 1] = std.math.max(1, dp[m - 1][j] - dungeon[m - 1][j - 1]);
+        dp[m - 1][j - 1] = @max(1, dp[m - 1][j] - dungeon[m - 1][j - 1]);
     }
 
     i = m - 1;
     while (i > 0) : (i -= 1) {
         j = n - 1;
         while (j > 0) : (j -= 1) {
-            const minHealthOnExit = std.math.min(dp[i][j - 1], dp[i - 1][j]);
-            dp[i - 1][j - 1] = std.math.max(1, minHealthOnExit - dungeon[i - 1][j - 1]);
+            const minHealthOnExit = @min(dp[i][j - 1], dp[i - 1][j]);
+            dp[i - 1][j - 1] = @max(1, minHealthOnExit - dungeon[i - 1][j - 1]);
         }
     }
 
@@ -38,27 +39,33 @@ pub fn calculateMinimumHP(allocator: std.mem.Allocator, dungeon: [][]i32) !i32 {
 
 test "calculateMinimumHP returns 7" {
     const allocator = std.testing.allocator;
-    const dungeon = &[_][]i32{
-        &[_]i32{ -2, -3, 3 },
-        &[_]i32{ -5, -10, 1 },
-        &[_]i32{ 10, 30, -5 },
+
+    const dungeon_data = [_][3]i32{
+        [_]i32{ -2, -3, 3 },
+        [_]i32{ -5, -10, 1 },
+        [_]i32{ 10, 30, -5 },
     };
-    try std.testing.expectEqual(@as(i32, 7), try calculateMinimumHP(allocator, dungeon));
+
+    try std.testing.expectEqual(@as(i32, 7), try calculateMinimumHP(allocator, dungeon_data));
 }
 
 test "calculateMinimumHP returns 1" {
     const allocator = std.testing.allocator;
-    const dungeon = &[_][]i32{
-        &[_]i32{ 0 },
+
+    const dungeon_data = [_][1]i32{
+        [_]i32{ 0 },
     };
-    try std.testing.expectEqual(@as(i32, 1), try calculateMinimumHP(allocator, dungeon));
+
+    try std.testing.expectEqual(@as(i32, 1), try calculateMinimumHP(allocator, dungeon_data));
 }
 
 test "calculateMinimumHP returns 6" {
     const allocator = std.testing.allocator;
-    const dungeon = &[_][]i32{
-        &[_]i32{ -2, -3, 3 },
-        &[_]i32{ -5, -10, 1 },
+
+    const dungeon_data = [_][3]i32{
+        [_]i32{ -2, -3, 3 },
+        [_]i32{ -5, -10, 1 },
     };
-    try std.testing.expectEqual(@as(i32, 6), try calculateMinimumHP(allocator, dungeon));
+
+    try std.testing.expectEqual(@as(i32, 6), try calculateMinimumHP(allocator, dungeon_data));
 }
